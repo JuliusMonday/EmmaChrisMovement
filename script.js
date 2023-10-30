bcd7094a031d2b733f2f838f3dc5414cdbb0d0f9
Julius Monday <julietech3@gmail.com>
2023-10-30 17:05:00 +0100

    Second commit

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-alert(10)
+const menuIcon = document.querySelector(".menu-icon");
+menuIcon.addEventListener("click", function(){
+    const sideMenu = document.querySelector(".side-menu-bar");
+    sideMenu.style.display = "flex";
+});
+const menuIcon2 = document.querySelector(".close-icon");
+menuIcon2.addEventListener("click", function(){
+    const sideMenu = document.querySelector(".side-menu-bar");
+    sideMenu.style.display = "none";
+});
+
+const hideButton = document.querySelector(".hide-manifesto");
+const showButton = document.querySelector(".show-manifesto");
+
+document.addEventListener("DOMContentLoaded", function () {
+    const showButton = document.querySelector(".show-manifesto");
+    const hideButton = document.querySelector(".hide-manifesto");
+    const manifestoText = document.querySelector(".manifesto-text");
+    const manifestoTextToShow = document.querySelector(".manifesto-text-to-show");
+
+    showButton.addEventListener("click", function () {
+        manifestoText.style.display = "none";
+        manifestoTextToShow.style.display = "block";
+    });
+
+    hideButton.addEventListener("click", function () {
+        manifestoText.style.display = "block";
+        manifestoTextToShow.style.display = "none";
+    });
+});
+
